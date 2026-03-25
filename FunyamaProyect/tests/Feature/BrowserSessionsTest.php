2b299ed2f02fe9de49bd908098ac929cf5e3ed26
hernanmedina <hernanchho@gmail.com>
2026-03-24 20:33:44 -0500

    nueva configuracion para jenkinsfile, BrowserSessionsTest, DeleteAccountTest

--- FunyamaProyect/tests/Feature/BrowserSessionsTest.php
+++ FunyamaProyect/tests/Feature/BrowserSessionsTest.php
@@ -15,6 +15,7 @@ class BrowserSessionsTest extends TestCase
     public function test_other_browser_sessions_can_be_logged_out(): void
     {
         $this->actingAs(User::factory()->create());
+        $this->withSession([]);
 
         Livewire::test(LogoutOtherBrowserSessionsForm::class)
             ->set('password', 'password')
