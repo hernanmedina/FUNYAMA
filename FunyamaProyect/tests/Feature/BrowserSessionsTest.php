3af271d2ccdbefb63c9d366ecbd14f3884cc5997
hernanmedina <hernanchho@gmail.com>
2026-03-24 20:44:11 -0500

    nueva configuracion para BrowserSessionsTest

--- FunyamaProyect/tests/Feature/BrowserSessionsTest.php
+++ FunyamaProyect/tests/Feature/BrowserSessionsTest.php
@@ -14,8 +14,15 @@ class BrowserSessionsTest extends TestCase
 
     public function test_other_browser_sessions_can_be_logged_out(): void
     {
-        $this->actingAs(User::factory()->create());
-        $this->withSession([]);
+        $user = User::factory()->create([
+            'password' => bcrypt('password'), // 👈 importante
+        ]);
+
+        $this->actingAs($user);
+
+        $this->withSession([
+            '_token' => csrf_token(),
+        ]);
 
         Livewire::test(LogoutOtherBrowserSessionsForm::class)
             ->set('password', 'password')
