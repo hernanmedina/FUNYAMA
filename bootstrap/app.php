280d6adec3c77172782cc68818b57c06e10723e4
hernanmedina <hernanchho@gmail.com>
2026-05-11 16:58:45 -0500

    configuracion de https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust Azure proxies - this ensures HTTPS is recognized correctly
+        $middleware->trustProxies(at: ['*']);
+        
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
