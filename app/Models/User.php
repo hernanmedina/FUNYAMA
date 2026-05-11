d3280fb5faee086b9f4b3f07450a3e3e7ec2e32a
hernanmedina <hernanchho@gmail.com>
2026-05-11 15:41:17 -0500

    creacion de nuevos campos para la migracion de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'apellido',
+        'documento_ID',
+        'role',
+        'telefono',
+        'direccion',
     ];
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('apellido')->nullable();
+            $table->string('documento_ID')->nullable();
+            $table->string('role')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('direccion')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
