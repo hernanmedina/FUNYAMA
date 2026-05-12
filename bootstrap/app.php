57a1c7a74f10a69eac705b60976b0c9770312f3e
hernanmedina <hernanchho@gmail.com>
2026-05-11 21:00:45 -0500

    Fix HTTPS issue: Add TrustProxies config and ForceHttps middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ForceHttps;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->web(ForceHttps::class);
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
---
+++ config/trustproxies.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Proxies
+    |--------------------------------------------------------------------------
+    |
+    | Set trusted proxy IP addresses.
+    |
+    | Both IPv4 and IPv6 addresses are
+    | supported, as well as CIDR notation.
+    |
+    | The "*" character is syntactic sugar
+    | within TrustedProxy to trust any proxy;
+    | a requirement when you cannot know the address
+    | of your proxy (e.g. if using Rackspace balancers).
+    |
+    */
+
+    'proxies' => '*',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Headers
+    |--------------------------------------------------------------------------
+    |
+    | The headers that should be trusted when using a proxy.
+    |
+    | Available options:
+    |   - HEADER_X_FORWARDED_ALL
+    |   - HEADER_X_FORWARDED_AWS_ELB
+    |   - HEADER_X_FORWARDED_FOR
+    |   - HEADER_X_FORWARDED_HOST
+    |   - HEADER_X_FORWARDED_PORT
+    |   - HEADER_X_FORWARDED_PROTO
+    |   - HEADER_FORWARDED
+    |
+    | Default: HEADER_X_FORWARDED_ALL
+    */
+
+    'headers' => \Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_ALL,
+
+];
