af0cb7a17f04977780f4c0199685cb2d57e7c201
hernanmedina <hernanchho@gmail.com>
2026-05-11 21:14:03 -0500

    Remove ForceHttps middleware to prevent redirect loop in Azure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\ForceHttps;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -14,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(ForceHttps::class);
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
